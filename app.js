83d3b7be09d76de706d625b93b923f4384e7b851
LeandroGuarda <leandro.guarda86@gmail.com>
2024-10-10 16:41:16 -0300

    se armo model actor

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const contenidoRoutes = require('./routes/contenidoRoutes');
 const db = require('./conexion/database');
+const morgan = require('morgan');
 
 // Middlewares
 app.use(express.json());
 app.use('/contenido', contenidoRoutes);
+app.use(morgan('dev'))
+
+// Ruta para el home
+app.get('/', (req,res) =>{
+  res.send('Bienvenido a TrailerFlix!');
+ });
+
+ // Routes for CRUD
+ 
+
+
+
 
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
     
--- models/actor.js
+++ models/actor.js
@@ -1,2 +1,16 @@
 // Model for Actor
-    
+const { sequelize } = require('../conexion/connection')
+const { DataTypes } = require('sequelize')
+
+const Actor = sequelize.define({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+        nombre: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+})
+
